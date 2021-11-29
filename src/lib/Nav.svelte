30a1a5f0b77c5f8c6c3f9e4cb2c41baaee9cfd5d
satyam mishra <satyam.mishra975@gmail.com>
2021-11-29 15:26:11 +0530

    change in what i know section

--- src/lib/Nav.svelte
+++
@@ -1,3 +0,0 @@
-<script>
-    let nav_elements = [{''}]
-</script>
--- src/lib/WhatIKnow.svelte
+++ src/lib/WhatIKnow.svelte
@@ -1,20 +1,20 @@
 <script>
     let skills = {'Frontend' : ['Svelte','Svelte kit','HTML','CSS','Javascript'], 'Backend':['Fastify','Node js','Mongodb'],'Cloud':['AWS','Azure']}
 </script>
-<div class="bg-purple-500 text-white mt-5 p-5 shadow-lg rounded-lg animated animate-zoom-in">
+<div class="bg-purple-500 text-white mt-5 p-5 shadow-lg rounded-lg ">
     <span class="font-semibold text-xl">What I Know :</span>
     <div class="m-3 grid grid-cols-3 gap-3">
         
         {#each Object.entries(skills) as [key,val]}
-        <div>
-        <header class="font-bold">{key}</header>
-        <ol class="m-3">
-            {#each val as el}
-                {#key el}
-                    <li class="list-disc animated animate-zoom-in animate-delay-300">{el}</li>
-                {/key}
-            {/each}
-        </ol>
+        <div class="animated animate-zoom-in">
+            <header class="font-bold">{key}</header>
+            <ol class="m-3">
+                {#each val as el}
+                    {#key el}
+                        <li class="list-disc animated animate-zoom-in animate-delay-300">{el}</li>
+                    {/key}
+                {/each}
+            </ol>
         </div>
         {/each}
     </div> 
