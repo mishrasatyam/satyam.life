8ba0a559734d981303ea38300bd4e286259f7d58
satyam mishra <satyam.mishra975@gmail.com>
2022-02-09 12:44:46 +0530

    add transitions

--- src/lib/Experience.svelte
+++ src/lib/Experience.svelte
@@ -1,17 +1,17 @@
 <script>
     import experience from '$lib/db/experience.json'
-
+    import {scale,fly} from 'svelte/transition'
 </script>
 <div class="bg-purple-700 text-white mt-5 p-2 sm:p-5 shadow-lg rounded-lg " >
     <div class="m-2 ">      
         {#each experience as el}
         <div class="mt-4">
-            <header class="font-medium text-xl">{el.company}, <span class="font-bold italic underline">{el.post}</span></header>
-            <span class="text-xs">{el.city} , </span>
-            <span class="text-xs">{el.range}</span>
-            <p class="shadow-lg p-4 bg-purple-400 rounded-lg">
+            <header class="font-medium text-xl" in:scale={{delay:0}}>{el.company}, <span class="font-bold italic underline">{el.post}</span></header>
+            <span class="text-xs" in:fly={{delay:100,x:-20,y:-20}}>{el.city} , </span>
+            <span class="text-xs" in:scale={{delay:200}}>{el.range}</span>
+            <p class="shadow-lg p-4 bg-purple-500 rounded-lg" >
                 {#each el.points as point}
-                    <span class="block">{point}</span>
+                    <span class="block shadow-sm bg-purple-400 p-2 rounded-sm" in:fly={{delay:210,x:-40}}>{point}</span>
                     <br/>
                 {/each}
             </p>
