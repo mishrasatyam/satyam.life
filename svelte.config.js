103b486b33dd16d8e5234ff80e5f01a8162ae830
satyam mishra <satyam.mishra975@gmail.com>
2022-02-09 12:46:36 +0530

    add transitions

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,7 @@ import adapter from "@sveltejs/adapter-auto";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    target: "#svelte"
+    adapter: adapter()
   },
 
   preprocess: [
