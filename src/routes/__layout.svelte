d30c9a37b92818610690e85b0c91b6e4b03364f2
Satyam Mishra <satyam@velocity.in>
2024-06-14 08:48:48 +0530

    fix: deploy

--- src/routes/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script context="module">
-	export const hydrate = false;
-  export const router=false
-</script>
-<script>
-  import "../app.css";
-</script>
-
-<slot />
--- src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Contact from '$lib/Contact.svelte';
-    import Tabs from '$lib/Tabs.svelte';
-</script>
-<div class="grid place-items-center my-4 mx-2">
-    <h1 class="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">Satyam Mishra</h1>
-    <Contact/>
-    <Tabs/>
-</div>
